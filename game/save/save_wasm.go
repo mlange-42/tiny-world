50e399768e93a4d522d2f977edc19b58ca1feb57
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2024-03-02 23:28:01 +0100

    Fix: add a prefix to web version save key (#79)

--- game/save/save_wasm.go
+++ game/save/save_wasm.go
@@ -4,12 +4,14 @@ package save
 
 import "syscall/js"
 
+const saveGamePrefix = "tiny-world-save-"
+
 func saveToFile(folder, name string, jsData []byte) error {
 	_ = folder
 
 	data := js.ValueOf(string(jsData))
 	storage := js.Global().Get("localStorage")
-	storage.Call("setItem", name, data)
+	storage.Call("setItem", saveGamePrefix+name, data)
 
 	return nil
 }
