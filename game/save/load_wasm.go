dfa62142c0096322111483134c648e624257bef9
mlange-42 <martin_lange_@gmx.net>
2024-03-02 23:24:12 +0100

    add a prefix to web version save key

--- game/save/load_wasm.go
+++ game/save/load_wasm.go
@@ -3,6 +3,7 @@
 package save
 
 import (
+	"strings"
 	"syscall/js"
 
 	serde "github.com/mlange-42/arche-serde"
@@ -13,7 +14,7 @@ func loadWorld(world *ecs.World, folder, name string) error {
 	_ = folder
 
 	storage := js.Global().Get("localStorage")
-	jsData := storage.Call("getItem", name)
+	jsData := storage.Call("getItem", saveGamePrefix+name)
 
 	return serde.Deserialize([]byte(jsData.String()), world)
 }
@@ -27,7 +28,9 @@ func listSaveGames(folder string) ([]string, error) {
 	cnt := storage.Get("length").Int()
 	for i := 0; i < cnt; i++ {
 		key := storage.Call("key", i).String()
-		games = append(games, key)
+		if strings.HasPrefix(key, saveGamePrefix) {
+			games = append(games, strings.TrimPrefix(key, saveGamePrefix))
+		}
 	}
 
 	return games, nil
